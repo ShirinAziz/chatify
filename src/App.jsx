a9d1c93007bd539158c5e3dd2da0b0d86e874771
ShirinAziz <Shirin.aziz96@outlook.com>
2024-07-31 23:52:39 +0200

    Add Navbar component to global layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
     <>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -4,17 +4,18 @@ import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div className="flex items-center justify-between w-full p-6">
-      <div className="flex items-center gap-1">
-        <img src={icon} className="h-10 w-10" />
-        <h1 className="text-3xl cursor-pointer">
-          Let's<span className="text-orange-400">Talk</span>
-        </h1>
-      </div>
-      <Link to="/register">
-        {" "}
-        <button className="btn btn-primary md:px-8">Get Started</button>
+    <div className="flex items-center justify-between w-full p-6 bg-transparent">
+      <Link to="/">
+        <div className="flex items-center gap-1">
+          <img src={icon} className="h-10 w-10" />
+          <h1 className="text-4xl cursor-pointer">
+            Let's<span className="text-orange-400">Talk</span>
+          </h1>
+        </div>
       </Link>
+      {/*<Link to="/register">
+        <button className="btn btn-primary md:px-8">Get Started</button>
+      </Link> */}
     </div>
   );
 };
