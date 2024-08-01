6439420f9c5cadcc0905be7032e5bc25d291a286
ShirinAziz <Shirin.aziz96@outlook.com>
2024-08-02 00:00:53 +0200

    Added functionality to decode JWT tokens using jwt-decode library. Saved decoded JWT data in local storage upon successful authentication.

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router";
+import { AuthContext } from "./AuthContext";
 
 const SideNav = () => {
   const navigate = useNavigate();
+  const { setIsLoggedIn } = useContext(AuthContext);
 
   const handleLogout = () => {
+    setIsLoggedIn(false);
     localStorage.removeItem("authToken");
-    localStorage.removeItem("userId");
-    localStorage.removeItem("userName");
-    localStorage.removeItem("userAvatar");
+    localStorage.removeItem("user");
     navigate("/login"); // Navigera till inloggningssidan
   };
 
   return (
-    <div>
+    <div className="fixed top-0 left-0 w-64 h-full bg-gray-800 text-white shadow-lg">
       <button onClick={handleLogout} className="btn btn-primary md:px-8">
         LogOut
       </button>
