340f89d3a273d2d9134b840b2255d75290950bd8
ShirinAziz <Shirin.aziz96@outlook.com>
2024-08-01 15:50:18 +0200

    Created a chat component

---
+++ src/Components/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SideNav from "./SideNav";
+
+const Chat = () => {
+  return (
+    <div>
+      <SideNav />
+      Chat
+    </div>
+  );
+};
+
+export default Chat;
