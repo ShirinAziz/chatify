d9114425f63651bd196a6b5b6ef6015704f27a45
ShirinAziz <Shirin.aziz96@outlook.com>
2024-08-02 12:55:41 +0200

    Updated context to store user information

--- src/Components/AuthContext.jsx
+++ src/Components/AuthContext.jsx
@@ -2,10 +2,16 @@ import { createContext, useState } from "react";
 
 const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("authToken") ? true : false
+  );
+  console.log("isLoggedin", isLoggedIn);
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) ?? null
+  );
+  console.log("user ", user);
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
